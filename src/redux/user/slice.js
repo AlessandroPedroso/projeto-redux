5503f0fbd1fccc3da38d9497c5e70d899ecc5e6e
Your Name <alessanrospedroso@outlook.com>
2025-07-04 14:09:21 -0300

    Cadastrando endereço

--- src/redux/user/slice.js
+++ src/redux/user/slice.js
@@ -13,14 +13,46 @@ export const userSlice = createSlice({
       // console.log(state.user);
       console.log(action.payload);
 
+      if (action.payload.name.length <= 4) {
+        alert("PREENCHA UM NOME COM MAIS DE 4 LETRAS");
+        return { ...state };
+      }
+
       return {
         ...state,
         user: {
           name: action.payload.name,
           email: action.payload.email,
+          address: null,
+        },
+      };
+    },
+    logoutUser: (state) => {
+      return {
+        ...state,
+        user: null,
+      };
+    },
+    addAddress: (state, action) => {
+      if (action.payload.location === "" || action.payload.number === "") {
+        alert("Preencha todos os campos");
+        return { ...state };
+      }
+
+      if (state.user === null) {
+        alert("Faça o login para cadastrar um endereço");
+        return { ...state };
+      }
+
+      alert("Dados atualizados!");
+
+      return {
+        ...state,
+        user: {
+          ...state.user,
           address: {
-            location: "Rua dos devs",
-            number: 34,
+            location: action.payload.location,
+            number: action.payload.number,
           },
         },
       };
@@ -28,6 +60,6 @@ export const userSlice = createSlice({
   },
 });
 
-export const { createUser } = userSlice.actions;
+export const { createUser, logoutUser, addAddress } = userSlice.actions;
 
 export default userSlice.reducer;
